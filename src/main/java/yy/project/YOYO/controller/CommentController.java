5ebc9c8359d19e6e39b1c5097e1e44a7e35debf1
woojung Jung <lln0302@naver.com>
2022-06-08 10:32:22 +0900

    comment 컨트롤러// checkMeeting(comment) 수정

---
+++ src/main/java/yy/project/YOYO/controller/CommentController.java
@@ -0,0 +1,134 @@
+package yy.project.YOYO.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import yy.project.YOYO.argumentresolver.Login;
+import yy.project.YOYO.domain.Comment;
+import yy.project.YOYO.domain.Team;
+import yy.project.YOYO.domain.User;
+import yy.project.YOYO.domain.UserTeam;
+import yy.project.YOYO.service.CommentService;
+import yy.project.YOYO.service.TeamService;
+import yy.project.YOYO.service.UserService;
+import yy.project.YOYO.service.UserTeamService;
+import yy.project.YOYO.vo.CommentVO;
+import yy.project.YOYO.vo.MeetingVO;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static yy.project.YOYO.controller.MeetingController.meetingvo;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class CommentController {
+
+
+    private final CommentService commentService;
+    private final UserService userService;
+    private final TeamService teamService;
+    private final UserTeamService userTeamService;
+
+    @GetMapping("/viewMeeting/{tID}")
+    public String viewMeeting(@PathVariable("tID") Long tID,Model model,@Login User loginUser){
+        Team team = teamService.findBytID(tID);
+        System.out.println(team.getTeamName());
+        model.addAttribute("team",team);
+
+        List<UserTeam> ut = userTeamService.findByTID(tID);
+        List<String> userIDs = new ArrayList<>();
+        for(int i=0; i<ut.size(); i++){
+            if(!ut.get(i).getUser().getUserID().equals(loginUser.getUserID())) {
+                userIDs.add(ut.get(i).getUser().getUserID());
+            }
+        }
+        model.addAttribute("user", loginUser.getUserID());
+        model.addAttribute("userIDs",userIDs);
+        return "viewMeeting";
+    }
+
+    @ResponseBody
+    @GetMapping("/findComment")
+    public List<CommentVO> board() {
+
+        return commentService.findComment(meetingvo.getTID());
+
+    }
+
+    @ResponseBody
+    @GetMapping("/saveComment")
+    public CommentVO saveComment(@Login User loginUser, @RequestParam String comment) {
+
+        Comment comment1 = new Comment();
+        comment1.setCommentContent(comment);
+        comment1.setWriterUID(loginUser.getUID());
+        comment1.setTeam(teamService.findBytID2(meetingvo.getTID()));
+        Long cmID = commentService.save(comment1);
+        Comment resComment = commentService.findBycmID(cmID);
+
+        CommentVO vo = new CommentVO();
+        vo.setCmID(cmID);
+        vo.setWriterName(loginUser.getUserName());
+        vo.setCommentContent(comment);
+
+        return vo;
+    }
+
+
+    @ResponseBody
+    @GetMapping("/deleteCommentCheck")
+    public boolean deleteCommentCheck(@RequestParam Long cmID, @Login User loginUser) {
+        if (loginUser == null) {
+            return false;
+        }
+        //댓글 주인이거나, 그 글의 글쓴이거나 둘 중 하나
+        else if (loginUser.getUID() == commentService.findBycmID(cmID).getWriterUID()) {
+            return true;
+        } else return false;
+    }
+
+
+    @ResponseBody
+    @GetMapping("/deleteComment")
+    public boolean deleteComment(@RequestParam Long cmID) {
+
+        commentService.delete(cmID);
+
+        return true;
+    }
+
+    @ResponseBody
+    @GetMapping("/updateComment")
+    public boolean updateComment(@RequestParam Long cmID) {
+        return true;
+    }
+
+    @ResponseBody
+    @GetMapping("/updateCommentCheck")
+    public boolean updateCommentCheck(@RequestParam Long cmID, @Login User loginUser) {
+        if (loginUser == null) {
+            return false;
+        } else {
+            return commentService.updateCommentCheck(cmID, loginUser.getUID());
+        }
+    }
+
+
+    @ResponseBody
+    @GetMapping("/updateCommentSave")
+    public boolean updateCommentSave(@RequestParam Long cmID, @RequestParam String commentContent) {
+
+        commentService.updateCommentSave(cmID, commentContent);
+        return true;
+    }
+}
---
+++ src/main/java/yy/project/YOYO/service/CommentServiceImpl.java
@@ -0,0 +1,74 @@
+package yy.project.YOYO.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import yy.project.YOYO.domain.Comment;
+import yy.project.YOYO.repository.CommentRepository;
+import yy.project.YOYO.repository.UserRepository;
+import yy.project.YOYO.vo.CommentVO;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@Service
+public class CommentServiceImpl implements CommentService{
+
+    private final UserRepository userRepository;
+    private final CommentRepository commentRepository;
+
+    @Override
+    public List<CommentVO> findComment(Long tID) {
+        List<Comment> comments = commentRepository.findAll();
+        List<CommentVO> cvos = new ArrayList<>();
+        for(Comment c : comments){
+            if(c.getTeam().getTID()==tID){
+                CommentVO commentVO = new CommentVO();
+                commentVO.setCommentContent(c.getCommentContent());
+                commentVO.setCmID(c.getCmID());
+                commentVO.setTbID(c.getTeam().getTID());
+                commentVO.setWriterName(userRepository.findByuID(c.getWriterUID()).getUserName());
+                cvos.add(commentVO);
+            }
+        }
+
+        return cvos;
+    }
+
+    @Override
+    public Long save(Comment comment1) {
+        Comment save = commentRepository.save(comment1);
+
+        return save.getCmID();
+
+    }
+
+    @Override
+    public Comment findBycmID(Long cmID) {
+        return commentRepository.findBycmID(cmID);
+    }
+
+    @Override
+    public void delete(Long cmID) {
+        commentRepository.delete(commentRepository.findBycmID(cmID));
+    }
+
+    @Override
+    public boolean updateCommentCheck(Long cmID, Long uid) {
+        if(commentRepository.findBycmID(cmID).getWriterUID()==uid){
+            return true;
+        }
+        else return false;
+    }
+
+
+    @Transactional
+    @Override
+    public void updateCommentSave(Long cmID, String commentContent) {
+        Comment c = commentRepository.findBycmID(cmID);
+        c.setCommentContent(commentContent);
+        commentRepository.save(c);
+    }
+}
